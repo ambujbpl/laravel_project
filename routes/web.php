d5825fb56694b9033aca7382f83ed7a958d9de99
ambujdubey <ambujdubey89@gmail.com>
2020-09-19 19:12:48 +0530

    create first route and send json response on it

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/harshuj', function () {
+  return ['name' => 'Harshuj', 'role' => 'Full Stack Software Developer', 'Location' => 'Bhopal'];
 });
